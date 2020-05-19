5da13a3897fc3c9684008978af1f0eb8d7c39091
Sandy Sihotang <sandysihotang12@gmail.com>
2020-05-19 11:25:39 +0700

    exist notif

--- frontend/src/components/AuthenticatedUser/Anggota/Layout/Notifikasi.vue
+++ frontend/src/components/AuthenticatedUser/Anggota/Layout/Notifikasi.vue
@@ -2,24 +2,40 @@
   <div>
     <q-page-container>
       <div class="q-pa-md">
-        <q-intersection
-          v-for="a in data"
-          transition="flip-right"
-          class="example-item"
-          :key="a.id"
-        >
-          <q-item clickable v-ripple>
-            <q-item-section avatar>
-              <q-avatar color="primary" text-color="white">
-                <q-icon name="report"/>
-              </q-avatar>
-            </q-item-section>
+        <div v-if="exist">
+          <q-intersection
+            v-for="a in data"
+            transition="flip-right"
+            class="example-item"
+            :key="a.id"
+          >
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-avatar color="primary" text-color="white">
+                  <q-icon name="report"/>
+                </q-avatar>
+              </q-item-section>
 
-            <q-item-section class="text-black">
-              <q-item-label caption>{{ a.pesan }}</q-item-label>
-            </q-item-section>
-          </q-item>
-        </q-intersection>
+              <q-item-section class="text-black">
+                <q-item-label caption>{{ a.pesan }}</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-intersection>
+        </div>
+        <div v-else>
+          <q-card>
+            <q-card-section>
+              <center>
+                <div class="text-purple">
+                  <q-icon name="fa fa-smile" size="xl"/>
+                </div>
+                <div class="text-black">
+                  Anda Tidak mempunyai Notifikasi
+                </div>
+              </center>
+            </q-card-section>
+          </q-card>
+        </div>
       </div>
     </q-page-container>
     <q-footer class="bg-white">
